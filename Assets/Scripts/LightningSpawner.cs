3c82bcda7eda948185c140b8af505128bdaef3c6
Liam-Angel <0622557@schoolsnet.act.edu.au>
2026-08-06 17:31:32 +1000

    borke

--- Assets/Scripts/LightningSpawner.cs
+++ Assets/Scripts/LightningSpawner.cs
@@ -4,60 +4,80 @@ using System.Collections;
 public class LightningSpawner : MonoBehaviour
 {
     [SerializeField] GameObject kaboom;
+    [SerializeField] LayerMask layers;
     Vector3 pos;
+
     public float interval;
 
     public float height;
+    public float boxheight;
     public float range;
+
+    public float floor;
 
     public float xmin;
     public float xmax;
     public float zmin;
     public float zmax;
 
-    private bool wait;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(DelayAction(interval));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (wait == false)
-        {
-            StartCoroutine(DelayAction(interval));
-            wait = true;
-        }
+  
     }
 
     IEnumerator DelayAction(float delayTime)
     {
-        yield return new WaitForSeconds(delayTime);
-
-        pos = new Vector3(Random.Range(xmin, xmax), height, Random.Range(zmin, zmax));
-        Collider[] hits = Physics.OverlapBox(pos, new Vector3(range, 80, range));
-        Vector3 peak = new Vector3(0, 0, 0);
-        float maxy = Mathf.NegativeInfinity;
-
-        foreach (Collider col in hits)
+        while(true)
         {
-            float topy = col.bounds.max.y;
-            if (topy > maxy)
-            {
-                maxy = topy;
-                peak = new Vector3(col.bounds.center.x, topy, col.bounds.center.z);
-            }
-        }
+            Collider hitcol = null;
+            yield return new WaitForSeconds(delayTime);
+            
+            pos = new Vector3(Random.Range(xmin, xmax), height, Random.Range(zmin, zmax));
+            Collider[] hits = Physics.OverlapBox(pos, new Vector3(range, boxheight, range), transform.rotation, layers);
+            Vector3 peak = new Vector3(0, 0, 0);
+            float maxy = -90;
 
-        Instantiate(kaboom, peak, transform.rotation);
-        wait = false;
+            
+
+            foreach (Collider col in hits)
+            {
+
+                float topy = col.bounds.max.y;
+                if (topy > maxy)
+                {
+                    maxy = topy;   
+                    hitcol = col;
+                    peak = new Vector3(col.bounds.center.x, maxy, col.bounds.center.z);
+                }
+            }
+
+            if (peak == new Vector3(0, 0, 0))
+            {
+                peak = new Vector3(pos.x, floor, pos.z);
+            }
+
+            if (hitcol != null && hitcol.gameObject.CompareTag("Player") != true)
+            {
+                Destroy (hitcol.gameObject);
+            }
+
+            print(peak);
+            Instantiate(kaboom, peak, transform.rotation);
+
+        }
     }
+        
 
     void OnDrawGizmos()
     {
         Gizmos.color = new Color(1f, 0.5f, 0f, 0.5f);
-        Gizmos.DrawCube(pos, new Vector3(range, 80, range));
+        Gizmos.DrawCube(pos, new Vector3(range * 2, boxheight * 2, range * 2));
     }
 }
