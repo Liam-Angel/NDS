f0d72436d5b9441c2afe0abaf9915d31c9c9107e
Liam-Angel <0622557@schoolsnet.act.edu.au>
2026-08-19 21:09:17 +1000

    water kinda

--- Assets/Scripts/WaterPhysics.cs
+++ Assets/Scripts/WaterPhysics.cs
@@ -7,6 +7,7 @@ public class WaterPhysics : MonoBehaviour
 {
     public float upforce;
     private List<GameObject> objects = new List<GameObject>();
+    private List<GameObject> removals = new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,29 +19,53 @@ public class WaterPhysics : MonoBehaviour
     {
         foreach (GameObject other in objects)
         {
-            Rigidbody objectrb = other.GetComponent<Rigidbody>();
-            Transform objecttr = other.transform;
-            Bounds bounds = GetComponent<Collider>().bounds;
+            print("wazzah");
+            if (other != null)
+            {
+                Rigidbody objectrb = other.GetComponent<Rigidbody>();
+                Transform objecttr = other.transform;
+                Bounds bounds = other.GetComponent<Collider>().bounds;
 
-            float miny = bounds.min.y;
-            float maxy = bounds.max.y;
-            float height = maxy - miny;
-            float volume = bounds.size.x * bounds.size.y * bounds.size.z;
+                float miny = bounds.min.y;
+                float maxy = bounds.max.y;
+                float height = maxy - miny;
+                float volume = bounds.size.x * bounds.size.y * bounds.size.z;
 
-            float depth = Mathf.Clamp(transform.position.y - miny, 0f, height);
-            float submerged = volume * (depth / height);
+                float depth = Mathf.Clamp(transform.position.y - miny, 0f, height);
+                float submerged = volume * (depth / height);
 
-            objectrb.AddForce(Vector3.up * submerged * upforce);
+                objectrb.AddForce(Vector3.up * submerged * upforce);
+                print(submerged);
+
+
+            }
+            else
+            {
+                removals.Add(other);
+            }
         }
-        objects.Clear();
-        
+        foreach (GameObject thing in removals)
+        {
+            objects.Remove(thing);
+            print("twin");
+        }
+        removals.Clear();
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.attachedRigidbody != null && other.attachedRigidbody.gameObject == other.gameObject)
-        {
+        if (other.attachedRigidbody != null && other.attachedRigidbody.gameObject == other.gameObject && !objects.Contains(other.gameObject))
+        { 
             objects.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(objects.Contains(other.gameObject))
+        {
+            objects.Remove(other.gameObject);
         }
     }
 }
